4e374cca3323e74b2f46c8802aff8cd217fe3907
Eli <78963198+niceEli@users.noreply.github.com>
2024-07-08 15:28:39 -0400

    chore: init

---
+++ src/vulPlay.cpp
@@ -0,0 +1,46 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(void)
+{
+
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Check if GLEW is ok after valid context */
+    /*if (glewInit() != GLEW_OK) {
+        cout << "Failed to initialize GLEW" << endl;
+    }*/
+    /* or Check if GLAD is ok after valid context */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+    glfwTerminate();
+    return 0;
+}
