bbe459b95827e1f2b5d224cc9bb890145595ffcd
Eli <78963198+niceEli@users.noreply.github.com>
2024-07-08 17:23:11 -0400

    feat: tutorial 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,20 @@
-﻿#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+﻿#include "first_app.hpp"
+
+// std
+
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 
 int run(int argc, char *argv[])
 {
-    GLFWwindow *window;
+    lve::FirstApp app {};
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
+    try {
+        app.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Check if GLEW is ok after valid context */
-    /*if (glewInit() != GLEW_OK) {
-        cout << "Failed to initialize GLEW" << endl;
-    }*/
-    /* or Check if GLAD is ok after valid context */
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-    }
-
-    glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
